2f26372416dede19fa74faf36b6528c886de4ead
AleksandarDrljaca <aleksandar.drljaca@student.etf.unibl.org>
2024-09-12 23:33:23 +0200

    del

--- ePJ2/src/main/java/org/unibl/etf/epj2/models/vehicle/TransportingPeople.java
+++
@@ -1,8 +0,0 @@
-package org.unibl.etf.epj2.models.vehicle;
-
-/**
- * Flag interface for indicating that car can transport multiple people
- * @author Aleksandar Drljaca
- */
-public interface TransportingPeople {
-}
